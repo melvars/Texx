9eca06d0f79c24618a0813edd8ab31f1c6532f20
Marvin Borner <marvin-borner@live.com>
2019-01-22 19:35:08 +0100

    Some events

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,27 @@
+const $ = require('jquery');
 const nanoid = require('nanoid');
 
 const userId = nanoid();
-const peer = new Peer(userId, {host: '127.0.0.1', port: 4242, path: '/'});
+const peer = new Peer(userId, {host: '127.0.0.1', port: 4242, path: '/', debug: 3});
 
-peer.on('open', id => {
-  console.log('[LOG] Your ID is ' + id)
+// LOG
+peer.on('open', id => console.log('[LOG] Your ID is ' + id));
+peer.on('connection', data => console.log('[LOG] Received data ' + data));
+peer.on('error', err => console.error(err));
+
+function connect(id) {
+    const connectionId = nanoid();
+    const conn = peer.connect(id, {label: connectionId});
+    console.log('[LOG] Your connection ID is ' + connectionId);
+
+    conn.on('open', function () {
+        conn.send('hi!');
+    });
+}
+
+/**
+ * Events after load
+ */
+$(document).ready(() => {
+    $('#user_id_form').on('click', e => connect($('#user_id').text));
 });
